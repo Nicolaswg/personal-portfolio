0f8977181a20fa8ba6e090d24ef6509cede73088
Nicolaswg <nigonbol123@gmail.com>
2022-07-04 12:27:19 -0400

    Implement final touch to portfolio card using dynamic js code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,6 +5,7 @@ import IMG2 from '../../assets/portfolio2.jpg';
 import IMG3 from '../../assets/portfolio3.jpg';
 import IMG4 from '../../assets/portfolio4.jpg';
 import IMG5 from '../../assets/portfolio5.jpg';
+import IMG6 from '../../assets/portfolio6.jpg';
 
 const cards = [
   {
@@ -24,30 +25,30 @@ const cards = [
   {
     id: 3,
     img: IMG3,
-    title: 'Crypto Coins',
+    title: 'Budget App',
     github: 'https://github.com/Nicolaswg/budget-app',
     demo: 'https://budget-capstone-1.herokuapp.com',
   },
   {
     id: 4,
-    img: IMG1,
+    img: IMG4,
     title: 'Pokemon List',
     github: 'https://github.com/Nicolaswg/Capstone_Pokemon_Api',
     demo: 'https://nicolaswg.github.io/Capstone_Pokemon_Api/',
   },
   {
     id: 5,
-    img: IMG1,
+    img: IMG5,
     title: 'RentUrTech',
     github: 'https://github.com/Final-Capstone-Elite-Group/RentUrTechFrontend',
     demo: 'https://renturtech.netlify.app',
   },
   {
     id: 6,
-    img: IMG1,
-    title: 'Crypto Coins',
-    github: 'https://github.com/Nicolaswg/crypto-capstone-project',
-    demo: 'https://nostalgic-boyd-fed032.netlify.app/',
+    img: IMG6,
+    title: 'Do List',
+    github: 'https://github.com/Nicolaswg/Do-List-Project',
+    demo: 'https://nicolaswg.github.io/Do-List-Project/',
   },
 ];
 
@@ -56,66 +57,22 @@ const Portfolio = () => (
     <h5>My Personal Repos</h5>
     <h2>Portfolio</h2>
     <div className="container portfolio-container">
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-      <article className="portfolio-item">
-        <div className="portfolio-img">
-          <img src={IMG1} alt="port-img" />
-        </div>
-        <h3>Crypto Coins</h3>
-        <div className="portfolio-cta">
-          <a href="https://github.com/Nicolaswg/crypto-capstone-project" className="btn" target="_blank" rel="noreferrer">Code</a>
-          <a href="https://nostalgic-boyd-fed032.netlify.app/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
+      {
+      cards.map(({
+        id, img, title, github, demo,
+      }) => (
+        <article className="portfolio-item" key={id}>
+          <div className="portfolio-img">
+            <img src={img} alt="port-img" />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio-cta">
+            <a href={github} className="btn" target="_blank" rel="noreferrer">Code</a>
+            <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+          </div>
+        </article>
+      ))
+      }
     </div>
   </section>
 );
